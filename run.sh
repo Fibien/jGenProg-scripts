279068154bc63c3fa3c200dc09c4ad149db143d1
Fibien <fabian.wiidh@gmail.com>
2024-03-22 09:25:18 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -24,9 +24,9 @@ fi
 #---- Functions
 
 # Checkout a bug at a location in tmp
-checkout_bug() { #args $1 Bug_category $2 Bug_number $3 Project_name $4 Mutation_rate $Pop
+checkout_bug() { #args $1 Bug_category $2 Bug_number $3 Project_name $4 Mutation_rate $5 Population_size $6 Iteration
 
-	local location="/${experiment_location}/${3}/${4}_${5}/${1}/${2}"
+	local location="/${experiment_location}/${3}/${6}/${4}_${5}/${1}/${2}"
 
     defects4j checkout -p "${1}" -v "${2}"b -w "${location}"
    
@@ -38,9 +38,9 @@ checkout_bug() { #args $1 Bug_category $2 Bug_number $3 Project_name $4 Mutation
 }
 
 # Search for a solution with jGenProg
-run_jgenprog() { #args $1 Bug_category $2 Bug_number $3 Project_name $4 Mutation_rate $5 Population_size $6 Seed
+run_jgenprog() { #args $1 Bug_category $2 Bug_number $3 Project_name $4 Mutation_rate $5 Population_size $6 Seed $7 Iteration
 
-	local bug_location="/${experiment_location}/${3}/${4}_${5}/${1}/${2}"
+	local bug_location="/${experiment_location}/${3}/${7}/${4}_${5}/${1}/${2}"
 	local log_location="/${experiment_location}/${3}/logs"
 	local filename="result_${4}_${5}_${1}_${2}.txt"
 	local dependency_location="${bug_location}/lib/"
@@ -114,7 +114,7 @@ create_folder(){ # $1 Folder location
 }
 
 # Extracts The results, time, and generation from the bug result textfile and save the row in a CSV file
-write_result(){ # args $1 Bug_category $2 Bug_number $3 Project_name $4 Mutation_rate $5 Pop
+write_result(){ # args $1 Bug_category $2 Bug_number $3 Project_name $4 Mutation_rate $5 Pop $6 Iteration
 	
 	local result_location="/${experiment_location}/${3}/logs/"
 	local filename="result_${4}_${5}_${1}_${2}.txt"	
@@ -130,43 +130,46 @@ write_result(){ # args $1 Bug_category $2 Bug_number $3 Project_name $4 Mutation
 		generation=$(grep -m 1 -o '^NR_GENERATIONS=.*' "${result_location}${filename}" | cut -d'=' -f2- | xargs)
 	fi 
 	
-	# !!!!!!!!! Lägg till fält som har med anledning till varför buggen inte lyckades
+	local status=$(grep -m 1 -o '^OUTPUT_STATUS=*' "${result_location}${filename}" | cut -d'=' -f2- | xargs)
 	
 	#mutation, population, category, bugg, solution (Found / Not Found), generation, time 
-	echo "${4},${5},${1},${2},${result},${generation},${time}" >> "/${experiment_location}/${3}/project_result.txt"
+	#echo "${4},${5},${1},${2},${result},${generation},${time},${status}" >> "/${experiment_location}/${3}/project_result.txt"
+
+	echo "${1},${2},${4},${5},${6},${generation},${time},${result},${status}" >> "/${experiment_location}/${3}/project_result.txt"
 }
 
-execute_bug_category(){ # args $1 Bug_category $2 Project_name $3 Mutation_rate $4 Population_size $5 Seed $6 bug_array 
+execute_bug_category(){ # args $1 Bug_category $2 Project_name $3 Mutation_rate $4 Population_size $5 Seed $6 Iteration $7 Bug_array 
 
 	local category="${1}"
 	local project_name="${2}"
 	local mutation_rate="${3}"
 	local population_size="${4}"
 	local seed="${5}"
-	local -n bug_array="${6}"
+	local iteration="${6}"
+	local -n bug_array="${7}"
 
 	for bug in "${bug_array[@]}"
     do
         checkout_bug "${category}" "${bug}" "${project_name}" "${mutation_rate}" "${population_size}"
 		run_jgenprog "${category}" "${bug}" "${project_name}" "${mutation_rate}" "${population_size}" "${seed}"
-		write_result "${category}" "${bug}" "${project_name}" "${mutation_rate}" "${population_size}"
+		write_result "${category}" "${bug}" "${project_name}" "${mutation_rate}" "${population_size}" "${iteration}"
     done
 }
 
-execute_math_bugs(){ # args $1 Project_name $2 Mutation_rate $3 Population_size $4 Seed
+execute_math_bugs(){ # args $1 Project_name $2 Mutation_rate $3 Population_size $4 Seed $5 iteration
 	#local math_bugs=(2 5 8 28 40 49 50 53 70 71 73 78 80 81 82 84 85 95)
 	local math_bugs=(53)
-	execute_bug_category Math "${1}" "${2}" "${3}" "${4}" math_bugs
+	execute_bug_category Math "${1}" "${2}" "${3}" "${4}" math_bugs "${5}"
 }
 
-execute_time_bugs(){ # args $1 Project_name $2 Mutation_rate $3 Population_size $4 Seed
+execute_time_bugs(){ # args $1 Project_name $2 Mutation_rate $3 Population_size $4 Seed $5 iteration
 	local time_bugs=(4 11)
-	execute_bug_category Time "${1}" "${2}" "${3}" "${4}" time_bugs
+	execute_bug_category Time "${1}" "${2}" "${3}" "${4}" time_bugs "${5}"
 }
 
-execute_chart_bugs(){ # args $1 Project_name $2 Mutation_rate $3 Population_size $4 Seed
+execute_chart_bugs(){ # args $1 Project_name $2 Mutation_rate $3 Population_size $4 Seed $5 iteration
 	local chart_bugs=(1 3 5 7 13 16 25)
-	execute_bug_category Chart "${1}" "${2}" "${3}" "${4}" chart_bugs
+	execute_bug_category Chart "${1}" "${2}" "${3}" "${4}" chart_bugs "${5}"
 }
 
 execute_bug_set(){ # $1 Project_name $2 Iteration $3 Seed
@@ -184,9 +187,9 @@ execute_bug_set(){ # $1 Project_name $2 Iteration $3 Seed
 	do
 		for population_size in "${population_size[@]}"
 		do
-			execute_math_bugs  "${project_name}" "${mutation_rate}" "${population_size}" "${seed}"
-			#execute_time_bugs  "${project_name}" "${mutation_rate}" "${population_size}" "${seed}"
-			#execute_chart_bugs "${project_name}" "${mutation_rate}" "${population_size}" "${seed}"
+			execute_math_bugs  "${project_name}" "${mutation_rate}" "${population_size}" "${seed}" "${iteration}"
+			#execute_time_bugs  "${project_name}" "${mutation_rate}" "${population_size}" "${seed}" "${iteration}"
+			#execute_chart_bugs "${project_name}" "${mutation_rate}" "${population_size}" "${seed}" "${iteration}"
 		done
 	done
 }
@@ -196,6 +199,7 @@ execute_iterations(){ # args $1 Project_name $2 Iterations $3 Seed
 	local project_name="${1}"
 	local result_location="/${experiment_location}/${project_name}"
 	local log_location="${result_location}/logs/"
+	local iterations="${2}"
 	
 	# Create the project and log folders
 	create_folder "${result_location}"
@@ -203,23 +207,20 @@ execute_iterations(){ # args $1 Project_name $2 Iterations $3 Seed
 	
 	echo "Mutation,Population,Category,BuggID,Solution,Generation,Time" >> "${result_location}/project_result.txt"
 	
-	local iterations="${2}"
-	
 	# when iteration with seq 1 is the first value
 	for i in $(seq ${iterations}); 
 	do
-		execute_bug_set() "${1}" i "${3}"
+		execute_bug_set() "${project_name}" i "${3}"
 	done
 }
 
 
-main() { # args $1 Project_name $2 Iterations´
+main() { # args $1 Project_name $2 Iterations
 	local seed=1
 	create_folder "${experiment_location}"
-	#execute_bug_set "${1}" "${2}" "${seed}" 
 	execute_iterations "${1}" "${2}" "${seed}" 
 }
 
 # ---- MAIN -----
 
-main "$1" "$2" #mandatory argument when invoking run.sh
+main "$1" "$2" #mandatory arguments when invoking run.sh
