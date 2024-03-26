85bfc7fff9fd009b28ed4ed8e9ca2de8bf7d2fdc
Fibien <fabian.wiidh@gmail.com>
2024-03-26 21:29:39 +0100

    fix pop array

--- run_valid_bugs_low_mutation_rate.sh
+++ run_valid_bugs_low_mutation_rate.sh
@@ -225,7 +225,7 @@ execute_bug_set(){ # $1 Iteration
 	local iteration="${1}"
 
 	local mutation_rates=(0.5 0.25)
-	local population_sizes=(1 10 20 30 40 50 100, 200, 300, 400, 600)
+	local population_sizes=(1 10 20 30 40 50 100 200 300 400 600)
 	
 	for mutation_rate in "${mutation_rates[@]}"
 	do
